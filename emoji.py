8acd6862606a6c3b97c0718b11afa5e096485001
Jackson Sommerich <github+jackson.sommerich@gmail.com>
2017-06-28 17:46:04 +1000

    Updated colour matching algorithm and added options for custom size emoji

--- emoji.py
+++ emoji.py
@@ -12,64 +12,66 @@ from PIL import Image
 from tqdm import tqdm
 
 class Emoji(object):
-    def __init__(self, image):
+    def __init__(self, image, size, transp):
         self.name = image
-        self.image = self._process_emoji(image)
+        self.image = self._process_emoji(size, transp)
         self.colour = self._set_colour()
 
-    def _process_emoji(self, image):
-        im = Image.open(image)
-        if im.size != (20, 20):
-            im.thumbnail((20, 20))
-        if args.transparency:
-            return im.convert("RGBA")
+    def _process_emoji(self, size, transp):
+        im = Image.open(self.name)
+        if im.size != (size, size):
+            im = im.resize((size, size), resample=Image.LANCZOS)
         alpha = im.convert('RGBA').split()[-1]
-        bg = Image.new("RGBA", im.size, (255, 255, 255, 255))
+        bg = Image.new("RGBA", im.size, (255, 255, 255, 0))
         bg.paste(im, mask=alpha)
+        if transp:
+            return bg
         return bg.convert("RGB")
 
     def _set_colour(self):
-        image = self.image.copy()
-        image = image.resize((1, 1), resample=Image.LANCZOS)
-        colour = image.getpixel((0, 0))
+        image = Image.open(self.name)
+        background = Image.new("RGBA", image.size, (255, 255, 255, 255))
+        background.paste(image, mask=image.convert('RGBA').split()[-1])
+        background = background.resize((1, 1), resample=Image.LANCZOS)
+        colour = background.getpixel((0, 0))
+        background.close()
         image.close()
         return colour
 
     def get_distance(self, pixel):
-        return ((self.colour[0] - pixel[0]) ** 2 +
-                (self.colour[1] - pixel[1]) ** 2 +
-                (self.colour[2] - pixel[2]) ** 2)
+        return (abs(self.colour[0] - pixel[0]) +
+                abs(self.colour[1] - pixel[1]) +
+                abs(self.colour[2] - pixel[2]))
 
     def get_emoji(self):
         return self.image
 
 
 class Picture(object):
-    def __init__(self, image, max_size, keeptransparency=False):
-        self.image = self._process_image(image, max_size, keeptransparency)
+    def __init__(self, image, max_size, emoji_size, transp):
+        self.image = self._process_image(image, max_size, transp)
         self.width, self.height = self.image.size
-        self.canvas_size = (self.image.width * 20, self.image.height * 20)
-        if keeptransparency:
-            self.canvas = Image.new("RGBA", self.canvas_size, color=(255, 255, 255, 0))
-        else:
-            self.canvas = Image.new("RGB", self.canvas_size, color=(255, 255, 255))
+        self.canvas_size = (self.image.width * emoji_size,
+                            self.image.height * emoji_size)
+        col_space = "RGBA" if transp else "RGB"
+        background = (255, 255, 255, 0) if transp else (255, 255, 255)
+        self.canvas = Image.new(col_space, self.canvas_size, color=background)
 
-    def _process_image(self, image, max_size, keeptransparency):
+    def _process_image(self, image, max_size, transp):
         im = Image.open(image)
         # Remove PNG transparency: https://stackoverflow.com/a/35859141
-        if im.mode in ("RGBA", "LA") or (im.mode == "P" and "transparency" in im.info):
+        if im.mode is "RGBA" or "LA" or (im.mode is "P" and "trans" in im.info):
             alpha = im.convert("RGBA").split()[-1]
             bg = Image.new("RGBA", im.size, (255, 255, 255, 255))
             bg.paste(im, mask=alpha)
             im = bg
         # Resize the input image keeping the aspect ratio correct
         im.thumbnail((max_size, max_size), resample=Image.LANCZOS)
-        if keeptransparency:
-            return im
-        return im.convert("RGB")
+        return im
 
     def paste_emoji(self, pos, emoji):
-        self.canvas.paste(emoji.get_emoji(), box=(pos[0] * 20, pos[1] * 20))
+        self.canvas.paste(emoji.get_emoji(),
+                          box=(pos[0] * emoji_size, pos[1] * emoji_size))
 
     def get_pixel(self, pos):
         return self.image.getpixel(pos)
@@ -97,6 +99,11 @@ if __name__ == "__main__":
                       help="Directory to retrieve emoji from.",
                       default="emoji")
 
+    args.add_argument("--emoji-size", "-d",
+                      type=int,
+                      help="The longest edge length of emoji you are using.",
+                      default=20)
+
     args.add_argument("--size", "-s",
                       type=int,
                       help="Maximum resolution for longest edge.",
@@ -106,26 +113,27 @@ if __name__ == "__main__":
                       help="Output image name.")
 
     args = args.parse_args()
+    emoji_size = args.emoji_size
+    transp = args.transparency
 
     # Create an emoji object for each png file in the emoji directory
-    emoji = [Emoji(os.path.join(args.emoji, x)) for x in
+    emoji = [Emoji(os.path.join(args.emoji, x), emoji_size, transp) for x in
         filter(lambda x: "png" in x, os.listdir(args.emoji))]
 
-    image = Picture(args.image, args.size, keeptransparency=args.transparency)
+    image = Picture(args.image, args.size, emoji_size, transp)
 
     print("Creating {}x{} image from {}x{} image and {} emoji:".format(
-        image.width * 20, image.height * 20, image.width, image.height,
-        len(emoji)))
+        image.width * emoji_size, image.height * emoji_size,
+        image.width, image.height, len(emoji)))
 
     previous_chars = dict()
 
     for y in tqdm(range(image.height), unit="rows"):
         for x in range(image.width):
             colour = image.get_pixel((x, y))
-            if args.transparency and colour[-1] >= 240 and\
-                colour[0] >= 240 and colour[1] >= 240 and colour[2] >= 240:
-                continue
-            elif not args.transparency and colour == (255, 255, 255):
+            # If a colour is very close to white then skip it, avoids weird
+            # edges when converting images with transparency.
+            if min(colour) >= 252:
                 continue
             try:
                 out_emoji = previous_chars[colour]
