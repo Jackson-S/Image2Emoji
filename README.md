5bbf4c2a9da10b560ae59cb5ff312a084ceeaf0d
Jackson Sommerich <github+jackson.sommerich@gmail.com>
2017-06-30 20:51:48 +1000

    Refactoring.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # 🖼➡️😊 (Image2Emoji)
 ### What is this?
-This script takes in an image file in any format Pillow supports, and outputs the same file but made up of emoji.
+This script takes in an image file in any format Pillow supports, and outputs
+the same file but made up of emoji.
 
 ![Imagine an image, but made of emoji, then insert it in this spot and be amazed.](sample.png "Emoji Sample")
 
@@ -8,11 +9,19 @@ This script takes in an image file in any format Pillow supports, and outputs th
 I needed more emoji in my life. One can never have enough emoji 👌
 
 ### Where do I get emoji from?
-You can use [this](https://github.com/tmm1/emoji-extractor) with the 20x20 emoji or if you lack a mac you can retrieve emoji from you can use the 32x32 png [emojione](https://www.emojione.com/developers/download) emoji set.
+If you're on MacOS then the program will automatically extract the included
+emoji from the system. Otherwise you can use the [emojione](https://www.emojione.com/developers/download) emoji set.
+
+### Requirements
+ - Python 2/3
+ - Pillow (recommended) or PIL
+ - tqdm
 
 ### Usage
 The program has been tested on MacOS, Windows and Ubuntu.
+If running on an OS other than MacOS then place emoji into a folder called
+"emoji", or specify a folder using the -e option.
 
-Once you have emoji place them into a folder called emoji (or specify the path with -e when running the script), then install the requirements with pip and run it in pypy or pypy3.
-If you have trouble installing pillow on pypy2/3 ensure libjpeg is installed and up to date on your computer.
-You _can_ use cpython but you will be waiting a **long** time for it to process.
+When installing pillow ensure that you have PNG support (requires zLib).
+
+I recommend you run this with pypy, cpython is a bit slow.
--- emoji.py
+++ emoji.py
@@ -1,116 +1,100 @@
-#! /usr/bin/env pypy
-
 """ Image to emoji converter - Jackson Sommerich 2017
-    This program is intended to be using pypy or pypy3, cpython is far too slow.
 """
 
 import os
 import re
 import sys
 import struct
-import platform
 import argparse
 import itertools
 
 from PIL import Image
 from tqdm import tqdm
 
-def extract_emoji(emoji_size):
-    """ Extracts emoji on OSX into an emoji folder. Return True if successful"""
 
-    # Get the path to the font file, OSX 10.12 changed to .ttc from .ttf
-    font_path = "/System/Library/Fonts/Apple Color Emoji"
-    if os.uname()[0] == "Darwin":
-        if int(platform.mac_ver()[0].split(".")[1]) >= 12:
-            font_path += ".ttc"
-        else:
-            font_path += ".ttf"
+class EmojiExtractor(object):
+    """Finds PNG images in Apple Color Emoji.tt(c/f) and extracts them"""
+    def __init__(self):
+        self.font = self._read_in_font()
 
-    if font_path is None or not os.path.exists(font_path):
-        return False
+    def _read_in_font(self):
+        font_path = "/System/Library/Fonts/"
+        font_name = ("Apple Color Emoji.ttf", "Apple Color Emoji.ttc")
 
-    with open(font_path, "rb") as font_file:
-        font = font_file.read()
+        # Get the path to the font file, OSX 10.12 changed to .ttc from .ttf
+        for name in font_name:
+            if os.path.exists(os.path.join(font_path, name)):
+                with open(os.path.join(font_path, name), "rb") as font_file:
+                    return font_file.read()
 
-    # Fetch locations of all PNG headers in font file
-    header_location = re.finditer(b'\x89PNG\r\n\x1a\n', font)
-    writing = False
-    emoji_array = list()
+        raise FileNotFoundError("Could not find Apple Color Emoji font!")
 
-    while True:
-        if not writing:
-            # Go to next occurence of png header
-            try:
-                index = next(header_location).start()
-            except StopIteration:
+    def _extract_png(self, position):
+        index = position
+        # Calculate the chunk size, but getting the data size and adding
+        # the metadata size to it (20 for IHDR and 12 for other chunks).
+        # Uses struct.unpack to convert from bytes to unsigned int (>I).
+        chunk_size = struct.unpack(">I", self.font[index+8:index+12])[0] + 20
+        emoji = bytearray(self.font[index:index+chunk_size])
+        index += chunk_size
+
+        while True:
+            chunk_size = struct.unpack(">I", self.font[index:index+4])[0] + 12
+
+            # Check for the IEND (Final) block
+            if self.font[index+4:index+8] == b"IEND":
+                emoji.extend(self.font[index:index+chunk_size])
                 break
+            # For all blocks between IHDR and IEND
+            else:
+                emoji.extend(self.font[index:index+chunk_size])
+                index += chunk_size
 
-            # Calculate the chunk size, but getting the data size and adding
-            # the metadata size to it (20 for IHDR and 12 for other chunks).
-            # Uses struct.unpack to convert from bytes to unsigned int (>I).
-            chunk_size = struct.unpack(">I", font[index+8:index+12])[0] + 20
+        return bytes(emoji)
 
-            # Ensure the correct size emoji is selected by reading the width
-            if emoji_size != struct.unpack(">I", font[index+16:index+20])[0]:
-                continue
-
-            writing = True
-            emoji_array.append(bytearray(font[index:index+chunk_size]))
-
-            index += chunk_size
-
-        # Check for the IEND (Final) block
-        elif writing and font[index+4:index+8] == b"IEND":
-            chunk_size = struct.unpack(">I", font[index:index+4])[0] + 12
-            emoji_array[-1].extend(font[index:index+chunk_size])
-            emoji_array[-1] = bytes(emoji_array[-1])
-            writing = False
-
-            index += chunk_size
-
-        # For all blocks between IHDR and IEND
-        elif writing:
-            chunk_size = struct.unpack(">I", font[index:index+4])[0] + 12
-            emoji_array[-1].extend(font[index:index+chunk_size])
-
-            index += chunk_size
-
-    if len(emoji_array) != 0:
-        # Output all emoji into a folder
+    def extract_emoji(self, size):
+        """Extracts emoji into ./emoji, only works for OSX"""
+        # Create the output folder
         if not os.path.exists("emoji"):
             os.mkdir("emoji")
-        for index, emoji in enumerate(emoji_array):
-            with open("emoji/{}.png".format(index), "wb") as out_file:
-                out_file.write(emoji)
-        return True
 
-    else:
-        return False
+        counter = 0
+
+        # Iterate through locations of all PNG headers in font file
+        for location in re.finditer(b'\x89PNG\r\n\x1a\n', self.font):
+            index = location.start()
+            if size == struct.unpack(">I", self.font[index+16:index+20])[0]:
+                file_path = os.path.join("emoji", "{}.png".format(counter))
+                counter += 1
+                with open(file_path, "wb") as out_file:
+                    out_file.write(self._extract_png(location.start()))
 
 
 class Emoji(object):
+    """ A single emoji object, contains emoji image, and functions for
+        determining colour of emoji
+    """
     def __init__(self, image, size, transp):
-        self.name = image
-        self.image = self._process_emoji(size, transp)
-        self.colour = self._set_colour()
+        self.image = self._process_emoji(image, size, transp)
+        self.colour = self._set_colour(image)
 
-    def _process_emoji(self, size, transp):
-        im = Image.open(self.name)
-        if im.size != (size, size):
+    def _process_emoji(self, image_path, size, transp):
+        image = Image.open(image_path)
+        if image.size != (size, size):
             try:
                 filter_algorithm = Image.LANCZOS
             except AttributeError:
                 filter_algorithm = Image.BICUBIC
-            im = im.resize((size, size), resample=filter_algorithm)
-        alpha = im.convert('RGBA').split()[-1]
-        bg = Image.new("RGBA", im.size, (255, 255, 255, 0))
-        bg.paste(im, mask=alpha)
+            image = image.resize((size, size), resample=filter_algorithm)
+        alpha = image.convert('RGBA').split()[-1]
+        new_image = Image.new("RGBA", image.size, (255, 255, 255, 0))
+        new_image.paste(image, mask=alpha)
         if transp:
-            return bg
-        return bg.convert("RGB")
+            return new_image
+        return new_image.convert("RGB")
 
-    def _set_colour(self):
-        image = Image.open(self.name)
+    def _set_colour(self, image_path):
+        image = Image.open(image_path)
         background = Image.new("RGBA", image.size, (255, 255, 255, 255))
         background.paste(image, mask=image.convert('RGBA').split()[-1])
         try:
@@ -133,30 +117,29 @@ class Emoji(object):
 
 
 class Picture(object):
-    def __init__(self, image, max_size, emoji_size, transp):
-        self.image = self._process_image(image, max_size, transp)
+    def __init__(self, image, max_size, emoji_size, transparency):
+        self.image = self._process_image(image, max_size)
         self.width, self.height = self.image.size
-        self.canvas_size = (self.image.width * emoji_size,
-                            self.image.height * emoji_size)
-        col_space = "RGBA" if transp else "RGB"
-        background = (255, 255, 255, 0) if transp else (255, 255, 255)
+        self.canvas_size = (self.width * emoji_size, self.height * emoji_size)
+        col_space = "RGBA" if transparency else "RGB"
+        background = (255, 255, 255, 0) if transparency else (255, 255, 255)
         self.canvas = Image.new(col_space, self.canvas_size, color=background)
 
-    def _process_image(self, image, max_size, transp):
-        im = Image.open(image)
+    def _process_image(self, image_path, max_size):
+        image = Image.open(image_path)
         # Remove PNG transparency: https://stackoverflow.com/a/35859141
-        if im.mode is "RGBA" or "LA" or (im.mode is "P" and "trans" in im.info):
-            alpha = im.convert("RGBA").split()[-1]
-            bg = Image.new("RGBA", im.size, (255, 255, 255, 255))
-            bg.paste(im, mask=alpha)
-            im = bg
+        if image.mode in ("RGBA", "LA") or (image.mode is "P" and "tran" in image.info):
+            alpha = image.convert("RGBA").split()[-1]
+            new_image = Image.new("RGBA", image.size, (255, 255, 255, 255))
+            new_image.paste(image, mask=alpha)
+            image = new_image
         # Resize the input image keeping the aspect ratio correct
         try:
             filter_algorithm = Image.LANCZOS
         except AttributeError:
             filter_algorithm = Image.BICUBIC
-        im.thumbnail((max_size, max_size), resample=filter_algorithm)
-        return im
+        image.thumbnail((max_size, max_size), resample=filter_algorithm)
+        return image
 
     def paste_emoji(self, pos, emoji):
         self.canvas.paste(emoji.get_emoji(),
@@ -165,13 +148,13 @@ class Picture(object):
     def get_pixel(self, pos):
         return self.image.getpixel(pos)
 
-    def save_canvas(self, location, format="png"):
+    def save_canvas(self, location, image_format="png"):
         # Save the image, type is determined by extension, or in case of no
         # extension is saved as png
         try:
             self.canvas.save(location)
         except ValueError:
-            self.canvas.save(location, format=format)
+            self.canvas.save(location, format=image_format)
 
 if __name__ == "__main__":
     # Arguments collection and processing
@@ -203,35 +186,39 @@ if __name__ == "__main__":
 
     args = args.parse_args()
     emoji_size = args.emoji_size
-    transp = args.transparency
 
-    if args.emoji is None and not os.path.exists("emoji"):
-        print("Attempting to extract emoji from emoji font...")
-        if extract_emoji(emoji_size) is False:
-            print("Failed to extract emoji (only works on MacOS). "
-                  "Specify a directory with pre-extracted emoji using -e to "
-                  "continue.")
-            sys.exit(1)
+    # If no emoji directory exists and none is specified
+    if args.emoji is None:
+        if not os.path.exists("emoji") or not os.listdir("emoji"):
+            print("Attempting to extract emoji from emoji font...")
+            try:
+                EmojiExtractor().extract_emoji(emoji_size)
+            except FileNotFoundError:
+                print("Failed to auto-extract emoji (only works on MacOS). "
+                      "Specify a directory with pre-extracted emoji using -e to "
+                      "continue.")
+                sys.exit(1)
 
-        print("Processing emoji...")
-        emoji = [Emoji(os.path.join("emoji", x), emoji_size, transp) for x in
-                 filter(lambda x: "png" in x, os.listdir("emoji"))]
-    elif args.emoji is not None:
-        # Create an emoji object for each png file in the emoji directory
-        emoji = [Emoji(os.path.join(args.emoji, x), emoji_size, transp) for x in
-                 filter(lambda x: "png" in x, os.listdir(args.emoji))]
-
+    # Make a directory listing of each png in the emoji directory
+    print("Processing emoji...")
+    if args.emoji is not None:
+        path_list = filter(lambda name: "png" in name, os.listdir(args.emoji))
+        path_list = map(lambda name: os.path.join(args.emoji, name), path_list)
     else:
-        emoji = [Emoji(os.path.join("emoji", x), emoji_size, transp) for x in
-                 filter(lambda x: "png" in x, os.listdir("emoji"))]
+        path_list = filter(lambda name: "png" in name, os.listdir("emoji"))
+        path_list = map(lambda name: os.path.join("emoji", name), path_list)
 
-    image = Picture(args.image, args.size, emoji_size, transp)
+    # Create emoji objects for each file in the listing
+    emoji = [Emoji(path, emoji_size, args.transparency) for path in path_list]
+
+    image = Picture(args.image, args.size, emoji_size, args.transparency)
 
     print("Creating {}x{} image from {}x{} image and {} emoji:".format(
         image.width * emoji_size, image.height * emoji_size,
         image.width, image.height, len(emoji)))
 
-    previous_chars = dict()
+    previous_colours = dict()
+    colour = None
 
     for y in tqdm(range(image.height), unit="rows"):
         for x in range(image.width):
@@ -241,10 +228,10 @@ if __name__ == "__main__":
             if min(colour) >= 252:
                 continue
             try:
-                out_emoji = previous_chars[colour]
+                out_emoji = previous_colours[colour]
             except KeyError:
                 out_emoji = min(emoji, key=lambda x: x.get_distance(colour))
-                previous_chars[colour] = out_emoji
+                previous_colours[colour] = out_emoji
             image.paste_emoji((x, y), out_emoji)
 
     output_path = args.output
