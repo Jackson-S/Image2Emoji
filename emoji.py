46679fdbc552bba8b1f3b284a774d4bccfaf3d34
Jackson Sommerich <github+jackson.sommerich@gmail.com>
2017-06-30 17:21:13 +1000

    Auto-Extract emoji on MacOS
    If running OSX, the program will now automatically extract emoji if
    none are present.

--- emoji.py
+++ emoji.py
@@ -5,11 +5,87 @@
 """
 
 import os
+import re
+import struct
+import platform
 import argparse
 import itertools
 
 from PIL import Image
 from tqdm import tqdm
+
+def extract_emoji(emoji_size):
+    """ Extracts emoji on OSX into an emoji folder. Return True if successful"""
+
+    # Get the path to the font file, OSX 10.12 changed to .ttc from .ttf
+    font_path = "/System/Library/Fonts/Apple Color Emoji"
+    if os.uname()[0] == "Darwin":
+        if int(platform.mac_ver()[0].split(".")[1]) >= 12:
+            font_path += ".ttc"
+        else:
+            font_path += ".ttf"
+
+    if font_path is None or not os.path.exists(font_path):
+        return False
+
+    with open(font_path, "rb") as font_file:
+        font = font_file.read()
+
+    # Fetch locations of all PNG headers in font file
+    header_location = re.finditer(b'\x89PNG\r\n\x1a\n', font)
+    writing = False
+    emoji_array = list()
+
+    while True:
+        if not writing:
+            # Go to next occurence of png header
+            try:
+                index = next(header_location).start()
+            except StopIteration:
+                break
+
+            # Calculate the chunk size, but getting the data size and adding
+            # the metadata size to it (20 for IHDR and 12 for other chunks).
+            # Uses struct.unpack to convert from bytes to unsigned int (>I).
+            chunk_size = struct.unpack(">I", font[index+8:index+12])[0] + 20
+
+            # Ensure the correct size emoji is selected by reading the width
+            if emoji_size != struct.unpack(">I", font[index+16:index+20])[0]:
+                continue
+
+            writing = True
+            emoji_array.append(bytearray(font[index:index+chunk_size]))
+
+            index += chunk_size
+
+        # Check for the IEND (Final) block
+        elif writing and font[index+4:index+8] == b"IEND":
+            chunk_size = struct.unpack(">I", font[index:index+4])[0] + 12
+            emoji_array[-1].extend(font[index:index+chunk_size])
+            emoji_array[-1] = bytes(emoji_array[-1])
+            writing = False
+
+            index += chunk_size
+
+        # For all blocks between IHDR and IEND
+        elif writing:
+            chunk_size = struct.unpack(">I", font[index:index+4])[0] + 12
+            emoji_array[-1].extend(font[index:index+chunk_size])
+
+            index += chunk_size
+
+    if len(emoji_array) != 0:
+        # Output all emoji into a folder
+        if not os.path.exists("emoji"):
+            os.mkdir("emoji")
+        for index, emoji in enumerate(emoji_array):
+            with open("emoji/{}.png".format(index), "wb") as out_file:
+                out_file.write(emoji)
+        return True
+
+    else:
+        return False
+
 
 class Emoji(object):
     def __init__(self, image, size, transp):
@@ -97,7 +173,7 @@ if __name__ == "__main__":
 
     args.add_argument("--emoji", "-e",
                       help="Directory to retrieve emoji from.",
-                      default="emoji")
+                      default=None)
 
     args.add_argument("--emoji-size", "-d",
                       type=int,
@@ -116,9 +192,25 @@ if __name__ == "__main__":
     emoji_size = args.emoji_size
     transp = args.transparency
 
-    # Create an emoji object for each png file in the emoji directory
-    emoji = [Emoji(os.path.join(args.emoji, x), emoji_size, transp) for x in
-        filter(lambda x: "png" in x, os.listdir(args.emoji))]
+    if args.emoji is None and not os.path.exists("emoji"):
+        print("Attempting to extract emoji from emoji font...")
+        if extract_emoji(emoji_size) is False:
+            print("Failed to extract emoji (only works on MacOS). "
+                  "Specify a directory with pre-extracted emoji using -e to "
+                  "continue.")
+            sys.exit(1)
+
+        print("Processing emoji...")
+        emoji = [Emoji(os.path.join("emoji", x), emoji_size, transp) for x in
+                 filter(lambda x: "png" in x, os.listdir("emoji"))]
+    elif args.emoji is not None:
+        # Create an emoji object for each png file in the emoji directory
+        emoji = [Emoji(os.path.join(args.emoji, x), emoji_size, transp) for x in
+                 filter(lambda x: "png" in x, os.listdir(args.emoji))]
+
+    else:
+        emoji = [Emoji(os.path.join("emoji", x), emoji_size, transp) for x in
+                 filter(lambda x: "png" in x, os.listdir("emoji"))]
 
     image = Picture(args.image, args.size, emoji_size, transp)
 
